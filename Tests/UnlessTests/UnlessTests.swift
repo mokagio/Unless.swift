e248c027dc626407a6f2322105578a1aeadb3ba2
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-10 22:00:43 +1100

    Add tests to ensure Swift syntax compatibility

--- Tests/UnlessTests/UnlessTests.swift
+++ Tests/UnlessTests/UnlessTests.swift
@@ -3,21 +3,36 @@ import XCTest
 
 class UnlessTests: XCTestCase {
   func testItCallsTheGivenClosureIfConditionIsFalse() {
-    var probe = false
-    unless({ return false }, do: { probe = true })
-    XCTAssertTrue(probe, "Expected closure to run and set probe to true")
+    var called = false
+    unless({ return false }, do: { called = true })
+    XCTAssertTrue(called, "Expected closure to run and set called to true")
   }
 
   func testItDoesNotCallTheGivenClosureIfConditionIsTrue() {
-    var probe = false
-    unless({ return true }, do: { probe = true })
-    XCTAssertFalse(probe, "Expected closure to not run")
+    var called = false
+    unless({ return true }, do: { called = true })
+    XCTAssertFalse(called, "Expected closure to not run")
+  }
+
+  func testWorksWithSwiftImplicitReturn() {
+    var called = false
+    unless({ false }, do: { called = true })
+    XCTAssertTrue(called, "Expected closure to run")
+  }
+
+  func testWorksWithSwiftImplicitReturnWithValue() {
+    var called = false
+    let value = false
+    unless({ value }, do: { called = true })
+    XCTAssertTrue(called, "Expected closure to run")
   }
 
   static var allTests : [(String, (UnlessTests) -> () throws -> Void)] {
     return [
       ("testItCallsTheGivenClosureIfConditionIsFalse", testItCallsTheGivenClosureIfConditionIsFalse),
-      ("testItDoesNotCallTheGivenClosureIfConditionIsTrue", testItDoesNotCallTheGivenClosureIfConditionIsTrue)
+      ("testItDoesNotCallTheGivenClosureIfConditionIsTrue", testItDoesNotCallTheGivenClosureIfConditionIsTrue),
+      ("testWorksWithSwiftImplicitReturn", testWorksWithSwiftImplicitReturn),
+      ("testWorksWithSwiftImplicitReturnWithValue", testWorksWithSwiftImplicitReturnWithValue)
     ]
   }
 }
