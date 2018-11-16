5bded179d72e9c9d5d090e4d5b1b83805c5a30d3
ebaker355 <ebaker355@gmail.com>
2018-11-16 14:30:54 -0600

    Update tests

--- Tests/UnlessTests/UnlessTests.swift
+++ Tests/UnlessTests/UnlessTests.swift
@@ -1,38 +1,23 @@
 import XCTest
 @testable import Unless
 
-class UnlessTests: XCTestCase {
-  func testItCallsTheGivenClosureIfConditionIsFalse() {
-    var called = false
-    unless({ return false }, do: { called = true })
-    XCTAssertTrue(called, "Expected closure to run and set called to true")
-  }
+final class UnlessTests: XCTestCase {
+    func testItCallsTheGivenClosureIfConditionIsFalse() {
+        var called = false
+        unless(false) { called = true }
+        XCTAssertTrue(called, "Expected closure to run and set called to true")
+    }
 
-  func testItDoesNotCallTheGivenClosureIfConditionIsTrue() {
-    var called = false
-    unless({ return true }, do: { called = true })
-    XCTAssertFalse(called, "Expected closure to not run")
-  }
+    func testItDoesNotCallTheGivenClosureIfConditionIsTrue() {
+        var called = false
+        unless(true) { called = true }
+        XCTAssertFalse(called, "Expected closure to not run")
+    }
 
-  func testWorksWithSwiftImplicitReturn() {
-    var called = false
-    unless({ false }, do: { called = true })
-    XCTAssertTrue(called, "Expected closure to run")
-  }
-
-  func testWorksWithSwiftImplicitReturnWithValue() {
-    var called = false
-    let value = false
-    unless({ value }, do: { called = true })
-    XCTAssertTrue(called, "Expected closure to run")
-  }
-
-  static var allTests : [(String, (UnlessTests) -> () throws -> Void)] {
-    return [
-      ("testItCallsTheGivenClosureIfConditionIsFalse", testItCallsTheGivenClosureIfConditionIsFalse),
-      ("testItDoesNotCallTheGivenClosureIfConditionIsTrue", testItDoesNotCallTheGivenClosureIfConditionIsTrue),
-      ("testWorksWithSwiftImplicitReturn", testWorksWithSwiftImplicitReturn),
-      ("testWorksWithSwiftImplicitReturnWithValue", testWorksWithSwiftImplicitReturnWithValue)
-    ]
-  }
+    static var allTests : [(String, (UnlessTests) -> () throws -> Void)] {
+        return [
+            ("testItCallsTheGivenClosureIfConditionIsFalse", testItCallsTheGivenClosureIfConditionIsFalse),
+            ("testItDoesNotCallTheGivenClosureIfConditionIsTrue", testItDoesNotCallTheGivenClosureIfConditionIsTrue)
+        ]
+    }
 }
