c8d45a5fc7e005ebe6da8253d77f01f731d9f7e9
ebaker355 <ebaker355@gmail.com>
2018-11-16 14:43:45 -0600

    2-space indents

--- Tests/UnlessTests/UnlessTests.swift
+++ Tests/UnlessTests/UnlessTests.swift
@@ -2,36 +2,36 @@ import XCTest
 @testable import Unless
 
 final class UnlessTests: XCTestCase {
-    func testItCallsTheGivenClosureIfConditionIsFalse() {
-        var called = false
-        unless(false) { called = true }
-        XCTAssertTrue(called, "Expected closure to run and set called to true")
-    }
+  func testItCallsTheGivenClosureIfConditionIsFalse() {
+    var called = false
+    unless(false) { called = true }
+    XCTAssertTrue(called, "Expected closure to run and set called to true")
+  }
 
-    func testItDoesNotCallTheGivenClosureIfConditionIsTrue() {
-        var called = false
-        unless(true) { called = true }
-        XCTAssertFalse(called, "Expected closure to not run")
-    }
+  func testItDoesNotCallTheGivenClosureIfConditionIsTrue() {
+    var called = false
+    unless(true) { called = true }
+    XCTAssertFalse(called, "Expected closure to not run")
+  }
 
-    func testItCallsTheGivenClosureIfAutoclosureConditionIsFalse() {
-        var called = false
-        unless(1 > 2) { called = true }
-        XCTAssertTrue(called, "Expected closure to run and set called to true")
-    }
+  func testItCallsTheGivenClosureIfAutoclosureConditionIsFalse() {
+    var called = false
+    unless(1 > 2) { called = true }
+    XCTAssertTrue(called, "Expected closure to run and set called to true")
+  }
 
-    func testItDoesNotCallTheGivenClosureIfAutoclosureConditionIsTrue() {
-        var called = false
-        unless(2 > 1) { called = true }
-        XCTAssertFalse(called, "Expected closure to not run")
-    }
+  func testItDoesNotCallTheGivenClosureIfAutoclosureConditionIsTrue() {
+    var called = false
+    unless(2 > 1) { called = true }
+    XCTAssertFalse(called, "Expected closure to not run")
+  }
 
-    static var allTests : [(String, (UnlessTests) -> () throws -> Void)] {
-        return [
-            ("testItCallsTheGivenClosureIfConditionIsFalse", testItCallsTheGivenClosureIfConditionIsFalse),
-            ("testItDoesNotCallTheGivenClosureIfConditionIsTrue", testItDoesNotCallTheGivenClosureIfConditionIsTrue),
-            ("testItCallsTheGivenClosureIfAutoclosureConditionIsFalse", testItCallsTheGivenClosureIfAutoclosureConditionIsFalse),
-            ("testItDoesNotCallTheGivenClosureIfAutoclosureConditionIsTrue", testItDoesNotCallTheGivenClosureIfAutoclosureConditionIsTrue)
-        ]
-    }
+  static var allTests : [(String, (UnlessTests) -> () throws -> Void)] {
+    return [
+      ("testItCallsTheGivenClosureIfConditionIsFalse", testItCallsTheGivenClosureIfConditionIsFalse),
+      ("testItDoesNotCallTheGivenClosureIfConditionIsTrue", testItDoesNotCallTheGivenClosureIfConditionIsTrue),
+      ("testItCallsTheGivenClosureIfAutoclosureConditionIsFalse", testItCallsTheGivenClosureIfAutoclosureConditionIsFalse),
+      ("testItDoesNotCallTheGivenClosureIfAutoclosureConditionIsTrue", testItDoesNotCallTheGivenClosureIfAutoclosureConditionIsTrue)
+    ]
+  }
 }
