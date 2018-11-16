ed77c877174b5f362f81eb099fe462bf9c11e130
ebaker355 <ebaker355@gmail.com>
2018-11-16 14:41:22 -0600

    Revert to guard statement

--- Sources/Unless.swift
+++ Sources/Unless.swift
@@ -1,7 +1,9 @@
 /// Execute the given closure only if the result of the first argument
 /// is false
 func unless(_ condition: @autoclosure () -> Bool, _ closure: () -> Void) {
-    if condition() == false {
-        closure()
+    guard condition() == false else {
+        return
     }
+
+    closure()
 }
