66004f0f6eb0036e3ae0087c07156f61ac8b7c83
ebaker355 <ebaker355@gmail.com>
2018-11-16 14:42:14 -0600

    2-space indents

--- Sources/Unless.swift
+++ Sources/Unless.swift
@@ -1,9 +1,9 @@
 /// Execute the given closure only if the result of the first argument
 /// is false
 func unless(_ condition: @autoclosure () -> Bool, _ closure: () -> Void) {
-    guard condition() == false else {
-        return
-    }
+  guard condition() == false else {
+    return
+  }
 
-    closure()
+  closure()
 }
